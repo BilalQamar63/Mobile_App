ab5c8653dff152fc51f175f845cbc23a3bade095
bilalqamar63 <bilalqamardeen@gmail.com>
2024-07-21 11:43:18 +0500

    Almost Done

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Stack_Navigate from './src/Navigation/Stack_Navigate'
+
+
+const App = () => {
+  return <Stack_Navigate />
+}
+
+export default App
+
+const styles = StyleSheet.create({})
